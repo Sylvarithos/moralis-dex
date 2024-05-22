dc55aa6ae5a26116f7cd48237038a625df534a42
devtopstar0201 <mont.skidev0201@gmail.com>
2024-05-22 11:55:41 -0700

    front-header

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import Header from "./components/Header";
+import Swap from "./components/Swap";
+import Tokens from "./components/Tokens";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="mainWindow">
+        <Routes>
+          <Route path="/" element={<Swap />} />
+          <Route path="/tokens" element={<Tokens />} />
+        </Routes>
+      </div>
+    </div>
+  );
+  // return (
+  //   <div className="App">
+  //     <Header connect={connect} isConnected={isConnected} address={address} />
+  //     <div className="mainWindow">
+  //       <Routes>
+  //         <Route
+  //           path="/"
+  //           element={<Swap isConnected={isConnected} address={address} />}
+  //         />
+  //         <Route path="/tokens" element={<Tokens />} />
+  //       </Routes>
+  //     </div>
+  //   </div>
+  // );
+}
+
+export default App;
---
+++ src/components/Swap.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { Input, Popover, Radio, Modal, message } from "antd";
+import {
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+
+function Swap() {
+  const [slippage, setSlippage] = useState(2.5);
+  const [tokenOneAmount, setTokenOneAmount] = useState(null);
+  const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeToken, setChangeToken] = useState(1);
+
+  function handleSlippageChange(e) {
+    setSlippage(e.target.value);
+  }
+
+  const settings = (
+    <>
+      <div>Slippage Tolerance</div>
+      <div>
+        <Radio.Group value={slippage} onChange={handleSlippageChange}>
+          <Radio.Button value={0.5}>0.5%</Radio.Button>
+          <Radio.Button value={2.5}>2.5%</Radio.Button>
+          <Radio.Button value={5}>5.0%</Radio.Button>
+        </Radio.Group>
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      <div className="tradeBox">
+        <div className="tradeBoxHeader">
+          <h4>Swap</h4>
+          <Popover
+            content={settings}
+            title="Settings"
+            trigger="click"
+            placement="bottomRight"
+          >
+            <SettingOutlined className="cog" />
+          </Popover>
+        </div>
+      </div>
+      <div className="inputs">
+        <Input
+          placeholder="0"
+          value={tokenOneAmount}
+          onChange={changeAmount}
+          disabled={!prices}
+        />
+        <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
+        <div className="switchButton" onClick={switchTokens}>
+          <ArrowDownOutlined className="switchArrow" />
+        </div>
+        <div className="assetOne" onClick={() => openModal(1)}>
+          <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
+          {tokenOne.ticker}
+          <DownOutlined />
+        </div>
+        <div className="assetTwo" onClick={() => openModal(2)}>
+          <img src={tokenTwo.img} alt="assetOneLogo" className="assetLogo" />
+          {tokenTwo.ticker}
+          <DownOutlined />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Swap;
